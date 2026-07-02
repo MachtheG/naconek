2f812c9d08580fd86b464e555a0b8781734b58ae
MachtheG <machndeg255@gmail.com>
2026-07-03 02:31:54 +0300

    fix: enforce community builder layer and remove static html override

--- api/index.php
+++ api/index.php
@@ -5,15 +5,12 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// 1. Fixed path to storage framework
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// 2. Fixed path to vendor autoloader
 require __DIR__.'/../vendor/autoload.php';
 
-// 3. Fixed path to bootstrap app
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
