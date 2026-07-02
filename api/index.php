ed35591de8815adfddd778abeb27eef1238ac64c
MachtheG <machndeg255@gmail.com>
2026-07-03 02:28:10 +0300

    fix: adjust laravel bootstrap paths for serverless engine compliance

--- api/index.php
+++ api/index.php
@@ -5,15 +5,15 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode.
+// 1. Fixed path to storage framework
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader.
+// 2. Fixed path to vendor autoloader
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request.
+// 3. Fixed path to bootstrap app
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
